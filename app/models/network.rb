cdf335e1d333c5d1da4abaa402274a1c5fca0dcb
Eugene <eugeneayonga@gmail.com>
2022-08-28 18:03:17 +0300

    commit tree d0029d3c6b8706218e31be2ac2bca83aa3275b4e

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,14 @@
 class Network < ActiveRecord::Base
-  
+  has_many :shows
+
+    def sorry
+        "We're sorry about passing on John Mulaney's pilot"
+    end
+
+    def shows
+        self.shows.collect do |show|
+            "#{show.name}"
+        end.join(", ")
+    end
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+
+    def actors_list
+        self.actors.collect do |character|
+            "#{character.first_name} #{character.last_name}"
+        end.join(", ")
+    end
+
 end
